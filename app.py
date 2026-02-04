ed900a6e57d229292cd70fd1b80b4fd1f3f5550d
kabir khan87 <kabirkhan80k@gmail.com>
2026-02-04 09:43:00 +0530

    linking is done

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-     return render_template('index.html')
- 
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run()
